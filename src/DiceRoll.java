c5c484d9b499c3e7c4ca1ffe1c82cbe5965f9974
Tanay Shah <tanay.v.shah@gmail.com>
2020-09-08 09:46:27 -0700

    function, user input, if

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class DiceRoll {
     public static void main(String[] args) {
-        System.out.println("bruh");
+        Scanner sc= new Scanner(System.in); //start input stream
+        System.out.println("Would you like to roll a dice? [y/n]: ");
+        String input= sc.nextLine();
+        if (input.equals("y")) {
+            System.out.println("Proceeding with roll");
+            rollDice();
+        }
+        else {
+            System.out.println("Skipping dice roll");
+        }
+    }
+    static void rollDice() {
+        System.out.println("Temp"); //temp content for function
     }
 }
