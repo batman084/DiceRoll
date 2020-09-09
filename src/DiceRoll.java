18587cd53390e9ab7aacd974575a94f93082c41c
batman084 <awesomegautam1@gmai.com>
2020-09-09 08:34:56 -0700

    added loop when asking if user wants to use dice

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -3,16 +3,28 @@ import java.util.Random;
 
 public class DiceRoll {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in); //start input stream
-        System.out.println("Would you like to roll a dice? [y/n]: ");
-        String input= sc.nextLine();
-        if (input.equals("y")) {
-            System.out.println("Proceeding with roll");
-            rollDice();
+        OUTER_LOOP: //label
+        for (;;) {
+            Scanner sc= new Scanner(System.in); //start input stream
+            System.out.println("Would you like to roll a dice? [y/n]: ");
+            String input= sc.nextLine();
+            LOOP: //label
+            if (input.equals("y")) {
+                System.out.println("Proceeding with roll");
+                rollDice();
+            }
+            else if (input.equals("n")) {
+                System.out.println("Skipping dice roll");
+                break OUTER_LOOP;
+            }
+            else {
+                System.out.println("Your input is not accepted. Please try again");
+                break LOOP;
+            }
         }
-        else {
-            System.out.println("Skipping dice roll");
-        }
+        System.out.println();
+        System.out.println("Thanks for using the Dice Roller!");
+
     }
     static void rollDice() {
         Random rand = new Random(); //Instance of new random class
