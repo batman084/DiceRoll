d53cdb1276ff1dfc288172240399cd069a808859
batman084 <awesomegautam1@gmai.com>
2020-09-09 08:24:05 -0700

    testing branches

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class DiceRoll {
     public static void main(String[] args) {
-        System.out.println("bruh");
+        int dice[];
+        dice = new int[]{1,2,3,4,5,6};
+
+        for (int i = 0; i < dice.length; i++){
+            System.out.println(dice[i]);
+        }
+
     }
 }
