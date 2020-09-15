1b593edf52ae0052a5ed13fb6505bd22f96c6885
dhruv <dhruv.sengupta04@gmail.com>
2020-09-15 08:58:55 -0700

    bruh

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -34,39 +34,121 @@ public class DiceRoll {
         switch (int_random) {
             case 0:
                 System.out.println();
+                rollAnimation();
                 break;
             case 1:
                 System.out.println();
+                rollAnimation();
                 break;
             case 2:
                 System.out.println();
+                rollAnimation();
                 break;
             case 3:
                 System.out.println();
+                rollAnimation();
                 break;
             case 4:
                 System.out.println();
+                rollAnimation();
                 break;
             case 5:
                 System.out.println();
+                rollAnimation();
                 break;
         }
-        rollAnimation();
+
         System.out.println("You rolled a " + diceFace[int_random]);
     }
     static void rollAnimation() {
+        Random rand = new Random();
+        for (int x =0; x<5;) {
+            int[] diceface = {1, 2, 3, 4, 5, 6};
+            int upperBound = 5;
+            int int_rando = rand.nextInt(upperBound);
+            int lastface = 100;
+            switch(int_rando) {
+                case 0:
+                    if (lastface == 1) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("|        |");
+                    System.out.println("|    0   |");
+                    System.out.println("|        |");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 1;
+                    break;
+                case 1:
+                    if (lastface == 2) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("|  0     |");
+                    System.out.println("|        |");
+                    System.out.println("|      0 |");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 2;
+                    break;
+
+                case 2:
+                    if (lastface == 3) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("|0       |");
+                    System.out.println("|   0    |");
+                    System.out.println("|       0|");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 3;
+                    break;
+
+                case 3:
+                    if (lastface == 4) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("| 0    0 |");
+                    System.out.println("|        |");
+                    System.out.println("| 0    0 |");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 4;
+                    break;
+                case 4:
+                    if (lastface == 5) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("| 0    0 |");
+                    System.out.println("|   0    |");
+                    System.out.println("| 0    0 |");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 5;
+                    break;
+                case 5:
+                    if (lastface == 6) {
+                        break;
+                    }
+                    System.out.println("----------");
+                    System.out.println("| 0    0 |");
+                    System.out.println("| 0    0 |");
+                    System.out.println("| 0    0 |");
+                    System.out.println("----------");
+                    x++;
+                    lastface = 6;
+                    break;
+
+            }
+
+        }
         //make dice faces only and cycle through them
-        System.out.println("                                                                                     *%&&&%(.                                                    ");
-        System.out.println();
-        System.out.println("                                                                                   %#            .(&%%*                                               ");
-        System.out.println();
-        System.out.println("                                                                               /&    .&&&&&%             *&&&#.                                      ");
-        System.out.println();
-        System.out.println("                                                                              &        .#/                       &&.                                  ");
-        System.out.println();
-        System.out.println("                                                                           &,                      #&&&&&%     &#  .&                                 ");
-        System.out.println();
-        System.out.println("                                                                        ##                                  %%       &.                               ");
+
+
 
 
 
