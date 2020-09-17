ac0ba3737ea0735a0f06fb6bc4105d194b43ad65
batman084 <awesomegautam1@gmai.com>
2020-09-17 09:09:27 -0700

    main menu done??

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -3,7 +3,27 @@ import java.util.Random;
 
 public class DiceRoll {
 
+
     public static void rollDice() {
+        Scanner scan= new Scanner(System.in); //start input stream
+        boolean loopBreak = true;
+        while (loopBreak) {
+            System.out.println("Would you like to roll a dice? [y/n]: ");
+            String input= scan.nextLine();
+            if (input.equals("y")) {
+                System.out.println("Proceeding with roll");
+                DiceRoll.rollDice();
+            }
+            else if (input.equals("n")) {
+                System.out.println("Skipping dice roll");
+                loopBreak = false;
+            }
+            else {
+                System.out.println("Your input is not accepted. Please try again");
+            }
+        }
+        System.out.println();
+        System.out.println("Thanks for using the Dice Roller!");
 
         Random rand = new Random(); //Instance of new random class
         int[] diceFace = {1, 2, 3, 4, 5, 6};
