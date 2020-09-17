8e7cf002d378640f2bf89e2c95a4947bc7aea7f3
Tanay Shah <tanay.v.shah@gmail.com>
2020-09-17 08:44:56 -0700

    to lower

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -8,16 +8,11 @@ public class RockPaperScissors {
         Scanner rps = new Scanner(System.in);
         System.out.println("Lets play Rock Paper Scissors. The computer will randomly chose an option. What do you chose? (Rock/Paper/Scissors) ");
         String userInput = rps.nextLine();
-        System.out.println("You chose " + userInput + ".");
-
+        String userLower = userInput.toLowerCase();
         Random computerValue = new Random();
 
 
-        switch (userInput) {
-            case "Rock":
-                rock();
-                System.out.println("You chose Rock");
-                break;
+        switch (userLower) {
             case "rock":
                 rock();
                 System.out.println("You chose Rock");
@@ -26,7 +21,7 @@ public class RockPaperScissors {
                 rock();
                 System.out.println("You chose Rock");
                 break;
-            case "Paper":
+            case "paper":
                 paper();
                 System.out.println("You chose Paper");
                 break;
@@ -34,19 +29,11 @@ public class RockPaperScissors {
                 paper();
                 System.out.println("You chose Paper");
                 break;
-            case "paper":
-                paper();
-                System.out.println("You chose Paper");
-                break;
-            case "Scissors":
+            case "scissors":
                 scissors();
                 System.out.println("You chose Scissors");
                 break;
             case "s":
-                scissors();
-                System.out.println("You chose Scissors");
-                break;
-            case "scissors":
                 scissors();
                 System.out.println("You chose Scissors");
                 break;
